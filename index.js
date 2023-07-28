cf257003f0857c753aa53c8302ec17077913978e
hikio-17 <fajri010217@gmail.com>
2023-07-29 01:30:23 +0700

    feat - create error global

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const app = express()
 
 const regionRoutes = require('./src/routes/regionRoute');
+const errorHandler = require('./src/middleware/errorHandler');
 
 require('dotenv').config()
 
@@ -14,6 +15,9 @@ app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 app.use('/api', regionRoutes);
+
+/** ERROR HANDLER */
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000
 
